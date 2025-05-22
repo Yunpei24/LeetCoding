6693c851c2dc3e5b84441fd5711cbd127ff76d38
Yunpei24 <nikiemajoshua24@gmail.com>
2025-05-22 16:47:31 +0000

    Solution of palindrome number problem

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from leetcode import LeetCode
 
 if __name__ == "__main__":
     leet_obj = LeetCode()
+
+    # Problem of Palindrome number Testing
+    n1 = 12121
+    leet_obj.isPalindrome(n1)
+
+    n2 = 321
+    leet_obj.isPalindrome(n2)
