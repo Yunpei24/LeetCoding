0554f2902ea859c0ebe5b7f3e0ecb0dd7b250832
Yunpei24 <nikiemajoshua24@gmail.com>
2025-05-22 15:54:26 +0000

    first commit

---
+++ leetcode.py
@@ -0,0 +1,15 @@
+"""
+* leetcode.py Contains the solution to LeetCode problems.
+"""
+
+class LeetCode:
+    """
+    A class to represent a solution to LeetCode problems.
+    """
+
+    def __init__(self):
+        """
+        Initializes the LeetCode class.
+        """
+        pass
+    
