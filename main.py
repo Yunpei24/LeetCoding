e959737e0169f6e322e6ad6ce730391cdbfba83b
Yunpei24 <nikiemajoshua24@gmail.com>
2025-06-09 11:43:47 +0000

    Problems isValid, lengthOfLastWord and mysqrt

--- main.py
+++ main.py
@@ -83,3 +83,33 @@ if __name__ == "__main__":
     print(f"The longest common prefix of {strs5} is: '{leet_obj.longestCommonPrefix(strs5)}'")  # Output should be "c"
     strs6 = ["ab", "a"]
     print(f"The longest common prefix of {strs6} is: '{leet_obj.longestCommonPrefix(strs6)}'")  # Output should be "a"
+
+    # Problem of Valid Parentheses Testing
+    s1 = "()"
+    print(f"The string '{s1}' has valid parentheses?: {leet_obj.isValid(s1)}")  # Output should be True
+    s2 = "()[]{}"
+    print(f"The string '{s2}' has valid parentheses?: {leet_obj.isValid(s2)}")  # Output should be True
+    s3 = "(]"
+    print(f"The string '{s3}' has valid parentheses?: {leet_obj.isValid(s3)}")  # Output should be False
+    s4 = "([)]"
+    print(f"The string '{s4}' has valid parentheses?: {leet_obj.isValid(s4)}")  # Output should be False
+    s5 = "{[]}"
+    print(f"The string '{s5}' has valid parentheses?: {leet_obj.isValid(s5)}")  # Output should be True
+
+    # Problem of length of Last Word Testing
+    s6 = "Hello World"
+    print(f"The length of the last word in '{s6}' is: {leet_obj.lengthOfLastWord(s6)}")  # Output should be 5
+    s7 = "   fly me   to   the moon  "
+    print(f"The length of the last word in '{s7}' is: {leet_obj.lengthOfLastWord(s7)}")  # Output should be 4
+    s8 = "Joshua is an engineer"
+    print(f"The length of the last word in '{s8}' is: {leet_obj.lengthOfLastWord(s8)}")  # Output should be 8
+
+    # Prblem of my sqrt Testing
+    x1 = 4
+    print(f"The square root of {x1} is: {leet_obj.mysqrt(x1)}")  # Output should be 2
+    x2 = 8
+    print(f"The square root of {x2} is: {leet_obj.mysqrt(x2)}")  # Output should be 2
+    x3 = 0
+    print(f"The square root of {x3} is: {leet_obj.mysqrt(x3)}")  # Output should be 0
+    x4 = 1
+    print(f"The square root of {x4} is: {leet_obj.mysqrt(x4)}")
